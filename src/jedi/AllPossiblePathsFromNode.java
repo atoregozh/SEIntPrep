b589bd6b2c80abae0578ebedcb4f9c4a11d4ac9a
Aizhan <toregozh@gmail.com>
2015-10-25 23:51:33 -0400

    doc comments added

--- src/jedi/AllPossiblePathsFromNode.java
+++ src/jedi/AllPossiblePathsFromNode.java
@@ -6,11 +6,31 @@ import java.util.*;
  * Created by aizhan on 10/25/15.
  */
 public class AllPossiblePathsFromNode {
+    /**
+     * Given chess element k (e.g. knight, pawn, king, queen, etc), find all possible 7-digit numbers based on the
+     * chess board positions it could go next
+     * Chess board in this question looks as following:
+     *         1  2  3
+     *         4  5  6
+     *         7  8  9
+     *            0
+     * @param k one of elements in a chess board, the rules where it can go next from startElem position
+     *          are prepopulated in a map
+     * @param startElem starting position of chess element within a chessboard
+     * @return all possible 7 digit numbers resulting from the paths chess element k could take. first three digits need
+     * to be separated from the last four digits with a dash
+     * e.x. output for findAllPossiblePaths(Knight k, char startElem) are:
+     * 349-2761, 349-4940, 343-4343, 349-2760, 343-4067, 340-4043, 340-4049, 340-6067...etc
+     */
+
+    /*
+    Solution concepts: permutation generation, DFS
+     */
 
     public static Set<String> findAllPossiblePaths(Knight k, char startElem) {
 
         Map<Character, ArrayList<Character>> graph = k.getPaths();
-        Stack<StringBuilder> stack = new Stack<StringBuilder>();
+        Stack<StringBuilder> stack = new Stack<>();
         Set<String> result = new HashSet<>();
         StringBuilder sb = new StringBuilder();
         stack.push(sb.append(startElem));
@@ -48,7 +68,7 @@ public class AllPossiblePathsFromNode {
 
 
 class Knight {
-    private static final Map<Character, ArrayList<Character>> knightPaths = new HashMap<Character, ArrayList<Character>>() {{
+    private static final Map<Character, ArrayList<Character>> knightRules = new HashMap<Character, ArrayList<Character>>() {{
         put('1', new ArrayList<>(Arrays.asList('8', '6')));
         put('2', new ArrayList<>(Arrays.asList('7', '9')));
         put('3', new ArrayList<>(Arrays.asList('4', '8')));
@@ -61,6 +81,6 @@ class Knight {
     }};
 
     public Map<Character, ArrayList<Character>> getPaths() {
-        return knightPaths;
+        return knightRules;
     }
 }
