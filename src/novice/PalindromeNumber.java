320323e870caa8d9ae6be416a3d201b74890aa3b
Aizhan <toregozh@gmail.com>
2019-02-22 14:07:39 -0500

    better solution with O(logn) time and constant space exists feelsbadman

--- src/novice/PalindromeNumber.java
+++ src/novice/PalindromeNumber.java
@@ -38,7 +38,7 @@ public class PalindromeNumber {
         if (x < 0) {
             return false;
         }
-        
+
         List<Integer> digits = new ArrayList<>();
 
         for (; x != 0; x /= 10) {
@@ -74,4 +74,30 @@ public class PalindromeNumber {
         System.out.println(isPalindrome(-1)); // expected: false
         //System.out.println(isPalindrome(1)); // expected: true
     }
+
+    /**
+     * MY SOLUTION WAS OVERTHINKING IT.
+     * there's a way to solve with with O(log(n)) time and O(1) space. 
+     *     public bool IsPalindrome(int x) {
+     *         // Special cases:
+     *         // As discussed above, when x < 0, x is not a palindrome.
+     *         // Also if the last digit of the number is 0, in order to be a palindrome,
+     *         // the first digit of the number also needs to be 0.
+     *         // Only 0 satisfy this property.
+     *         if(x < 0 || (x % 10 == 0 && x != 0)) {
+     *             return false;
+     *         }
+     *
+     *         int revertedNumber = 0;
+     *         while(x > revertedNumber) {
+     *             revertedNumber = revertedNumber * 10 + x % 10;
+     *             x /= 10;
+     *         }
+     *
+     *         // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+     *         // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+     *         // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+     *         return x == revertedNumber || x == revertedNumber/10;
+     *     }
+     */
 }
